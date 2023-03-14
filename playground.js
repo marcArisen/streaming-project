c12e494f6a2d74883847f2a80b0e8932e00a4b16
marcArisen <markjingnum9@gmail.com>
2023-03-14 10:14:39 +0700

    delete one folder

---
+++ playground.js
@@ -0,0 +1,4 @@
+console.log(Math.max([2,5,10]));
+var s =  Math.max(...[2,5,10]);
+console.log(s);
+console.log([2,5,10]);
---
+++ streaming-frontend/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <video-player
+    src="http://localhost:3030/hls/devito,360p.mp4,480p.mp4,720p.mp4,.en_US.vtt,.urlset/master.m3u8"
+    poster="/your-path/poster.jpg"
+    controls
+    :loop="true"
+    :volume="0.6"
+  />
+</template>
+
+<script>
+  import { defineComponent } from 'vue'
+  import { VideoPlayer } from '@videojs-player/vue'
+  import 'video.js/dist/video-js.css'
+
+  export default defineComponent({
+    components: {
+      VideoPlayer
+    }
+  })
+</script>
