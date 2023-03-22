8ac67be89b4643b9b426b8635ea128859c882b59
Sujinna Phutatham <gift@Sujinnas-MacBook-Pro.local>
2023-03-23 02:22:12 +0700

    fix gitignore + add vuetify

--- streaming-frontend/src/App.vue
+++ streaming-frontend/src/App.vue
@@ -1,39 +1,23 @@
 <template>
-  <div>
-    <VideoPlane :videoList="videoList"></VideoPlane>
-    <UploadBox></UploadBox>
-  </div>
+  <v-app>
+    <v-main>
+      <HelloWorld/>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import { defineComponent } from "vue";
-import UploadBox from "./components/Upload-Box-Component.vue";
-import VideoPlane from "./components/Video-Plane-Component.vue";
-import ApiService from "./services/ApiService.js";
+import HelloWorld from './components/HelloWorld.vue'
 
-export default defineComponent({
+export default {
+  name: 'App',
+
   components: {
-    UploadBox,
-    VideoPlane,
+    HelloWorld,
   },
-  data() {
-    return {
-      // mockVideoUrl: "http://localhost:3030/hls/devito,360p.mp4,480p.mp4,720p.mp4,.en_US.vtt,.urlset/master.m3u8",
-      // mockVideoUrl2: "http://localhost:3030/hls/thekid.mp4/master.m3u8",
-      // posterUrl: "none",
-      videoList: [],
-    };
-  },
-  // computed:{
-  //   async fetchVideos(){
-  //     this.videoList = await ApiService.getVideos();
-  //     console.log("CONSOLEEE: " + this.videoList);
-  //     return this.videoList
-  //   }
-  // },
-  async mounted() {
-    this.videoList = await ApiService.getVideos();
-    // console.log(this.videoList);
-  },
-});
+
+  data: () => ({
+    //
+  }),
+}
 </script>
