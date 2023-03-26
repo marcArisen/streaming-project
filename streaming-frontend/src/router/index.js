1c4407fb63ec37fbd91a92a1f7d7dab8836ef60c
Sujinna Phutatham <gift@Sujinnas-MacBook-Pro.local>
2023-03-26 12:48:50 +0700

    fix video list

--- streaming-frontend/src/router/index.js
+++ streaming-frontend/src/router/index.js
@@ -1,9 +1,15 @@
 import { createRouter, createWebHistory } from "vue-router";
+import ListVideoView from "../views/ListVideoView.vue";
 import UploadVideoView from "../views/UploadVideoView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    {
+      path: "/",
+      name: "listVideoView",
+      component: ListVideoView,
+    },
     {
       path: "/upload-video",
       name: "uploadVideoView",
---
+++ streaming-frontend/src/views/ListVideoView.vue
@@ -0,0 +1,34 @@
+<template>
+  <div v-for="video in videos" :key="video">
+    <video-player
+      :src="video"
+      controls
+      :loop="true"
+      :volume="0.6"
+      :width="800"
+      :height="640"
+    />
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import apiService from "../services/api-service";
+import { VideoPlayer } from "@videojs-player/vue";
+import "video.js/dist/video-js.css";
+
+export default defineComponent({
+  components: {
+    VideoPlayer,
+  },
+  data() {
+    return {
+      videos: [],
+    };
+  },
+  async mounted() {
+    this.videos = ["https://youtu.be/Lh-lcHfyEN0", "https://youtu.be/Lh-lcHfyEN0", "https://youtu.be/Lh-lcHfyEN0"]
+    // this.videos = await apiService.getVideos();
+  },
+});
+</script>
